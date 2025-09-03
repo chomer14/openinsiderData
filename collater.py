fd967e3f39b2d7cfdb4ccaf261f0d3d4c2c510e3
chomer14 <80199862+chomer14@users.noreply.github.com>
2025-09-03 21:38:32 +0100

    Add initial collater.py with data modeling notes
    Introduces collater.py containing comments outlining plans for database tables and data normalization, including insiders, companies, roles, and transaction attributes.

---
+++ collater.py
@@ -0,0 +1,8 @@
+# Generate table of insiders with [id | name] (SELECT DISTINCT insider_name FROM transactions_bronze)
+# Generate table of companies with [id | ticker | name]
+# Explore solutions to what role each user plays in a company
+# - Potentially another table that relates [transaction id | personnal role]
+# - Would require another table of possible roles (E.g. CFO, CEO, 10%...)
+
+# Remove X, filing date, ticker, company name, insider name, dOwnedPc
+# replace trade type with boolean
