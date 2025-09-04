aaac034945de387fc62c1c8d9741ef86e99a47d7
chomer14 <80199862+chomer14@users.noreply.github.com>
2025-09-04 15:00:58 +0100

    Add function to create and populate companies table
    Introduces create_companies_table to create the companies_gold table in SQLite and populate it with unique tickers and company names from transactions_bronze.

--- cleaner.py
+++ cleaner.py
@@ -7,3 +7,23 @@
 # Remove X, filing date, ticker, company name, insider name, dOwnedPc
 # replace trade type with boolean
 # add columns to relate insiders and companies
+import sqlite3
+from datetime import datetime
+def create_companies_table(cur: sqlite3.Cursor, conn: sqlite3.Connection):
+    cur.execute(""" CREATE TABLE "companies_gold" (
+                        "id"	    INTEGER NOT NULL UNIQUE,
+                        "ticker"	TEXT,
+                        "name"	    TEXT,
+                        PRIMARY KEY("id" AUTOINCREMENT)
+                    )""")
+    
+    # populate companies table
+    cur.execute("SELECT DISTINCT ticker FROM transactions_bronze")
+    tickers = cur.fetchall()
+    
+    for ticker in tickers:
+        ticker_symbol = ticker[0]
+        cur.execute("SELECT company_name FROM transactions_bronze WHERE ticker=?", (ticker_symbol,))
+        company_name = cur.fetchone()[0]
+        cur.execute("INSERT INTO companies_gold (ticker, name) VALUES (?, ?)", (ticker_symbol, company_name,))
+    conn.commit()
